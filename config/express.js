1a82dc21843580a25c9cf8ae19fbe5ac9eda2d31
Rafael Hiroshi <hiroshi_nuts@hotmail.com>
2017-11-13 16:58:42 -0200

    Primeiro Comit , Criacao do servidor , inclusao do template de Dashboard , criacao do banco de dados local

---
+++ config/express.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var consign = require('consign');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var app = express();
+
+//app.set('secret', 'homemavestruz'); 
+app.use(express.static('./public'));
+//app.use(bodyParser.json());
+
+consign({cwd: 'app'})
+    //.include('models')
+    //.then('api')
+    //.then('routes/auth.js')
+    .include('routes') //mudar para then quando colocar o login token
+    .into(app);
+
+module.exports = app;
